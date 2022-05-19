71ea17585e0567aa3ba8af09f24d300112fb3efa
Dima Nekrasov <dmitrij.nekrasov@gmail.com>
2022-05-19 17:29:12 +0400

    add middleware support

---
+++ foundation/webapp/middleware.go
@@ -0,0 +1,15 @@
+package webapp
+
+type Middleware func(Handler) Handler
+
+func wrapMiddleware(mw []Middleware, handler Handler) Handler {
+
+	for i := len(mw) - 1; i >= 0; i-- {
+		h := mw[i]
+		if h != nil {
+			handler = h(handler)
+		}
+	}
+
+	return handler
+}
