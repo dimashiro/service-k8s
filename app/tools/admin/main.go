ce9cbd39de1d2efd2a4cda6d104584dddefa439d
Dima Nekrasov <dmitrij.nekrasov@gmail.com>
2022-05-22 17:28:39 +0400

    playing with gen token and keys

--- app/tools/admin/main.go
+++ app/tools/admin/main.go
@@ -6,19 +6,84 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"io"
 	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func main() {
-	err := genKey()
+	err := genToken()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 }
 
+func genToken() error {
+	// temporary hardcoded
+	file, err := os.Open("private.pem")
+	if err != nil {
+		return err
+	}
+
+	privatePEM, err := io.ReadAll(io.LimitReader(file, 1024*1024))
+	if err != nil {
+		return fmt.Errorf("reading auth private key: %w", err)
+	}
+
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privatePEM)
+	if err != nil {
+		return fmt.Errorf("parsing auth private key: %w", err)
+	}
+
+	//__________________________________________________________________________
+
+	claims := struct {
+		jwt.StandardClaims
+		Roles []string
+	}{
+		StandardClaims: jwt.StandardClaims{
+			Issuer:    "retail-api project",
+			Subject:   "000000000",
+			ExpiresAt: time.Now().Add(8760 * time.Hour).Unix(),
+			IssuedAt:  time.Now().UTC().Unix(),
+		},
+		Roles: []string{"ADMIN"},
+	}
+
+	method := jwt.GetSigningMethod("RS256")
+	token := jwt.NewWithClaims(method, claims)
+	token.Header["kid"] = "developmentkeyid"
+
+	tokenStr, err := token.SignedString(privateKey)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("token: ", tokenStr)
+
+	// Marshal the public key from the private key to PKIX.
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("marshaling public key: %w", err)
+	}
+
+	publicBlock := pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+
+	if err := pem.Encode(os.Stdout, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	return nil
+}
+
 func genKey() error {
-	//_____________Gen private key______________________________
+	//___Gen private key________________________________________________________
 	prvKey, err := rsa.GenerateKey(rand.Reader, 2048)
 	if err != nil {
 		return err
@@ -40,7 +105,7 @@ func genKey() error {
 		return fmt.Errorf("encoding to private file: %w", err)
 	}
 
-	//_____________Gen publick key______________________________
+	//___Gen publick key________________________________________________________
 	// Marshal the public key from the private key to PKIX.
 	asn1Bytes, err := x509.MarshalPKIXPublicKey(&prvKey.PublicKey)
 	if err != nil {
--- go.mod
+++ go.mod
@@ -4,16 +4,17 @@ go 1.17
 
 require (
 	github.com/dimfeld/httptreemux/v5 v5.4.0
+	github.com/golang-jwt/jwt/v4 v4.4.1
 	github.com/ilyakaznacheev/cleanenv v1.2.6
-	go.uber.org/atomic v1.7.0
 	go.uber.org/automaxprocs v1.5.1
-	go.uber.org/multierr v1.6.0
 	go.uber.org/zap v1.21.0
 )
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
